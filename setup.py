72355491ea35e99bf2fcc780565692759efc3370
bouianpe <152520729+bouianpe@users.noreply.github.com>
2024-10-18 14:32:25 +0000

    Upgrade nox to 2024.10.09

--- setup.py
+++ setup.py
@@ -6,5 +6,5 @@ from setuptools import setup
 setup(
     name='pre_commit_placeholder_package',
     version='0.0.0',
-    install_requires=['nox==2022.11.21'],
+    install_requires=['nox==2024.10.09'],
 )
